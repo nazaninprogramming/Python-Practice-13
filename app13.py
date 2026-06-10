e24098a2fcc28a0c09d73775fed3d049769b2e61
Nazanin <nazaninprogramming@gmail.com>
2026-06-10 21:05:30 +0330

    Add files via upload

---
+++ app13.py
@@ -0,0 +1,9 @@
+""" Practice comparison operators. """
+print(3 == 3)
+print(3 < 4)
+print(4 > 4)
+print(2 <= 3)
+print(1 >= 3)
+print(3 != 3)
+print("nazanin" == "nazanin")
+print("NAZANIN" == "nazanin")
